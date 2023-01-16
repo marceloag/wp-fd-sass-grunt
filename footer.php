fb2ebfeb7fa0628b751ac501e8b18cb83954f1c6
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-01-16 16:36:30 -0300

    First Comit

--- footer.php
+++ footer.php
@@ -1,5 +1,19 @@
 <!-- FOOTER -->
     <footer>
+        <div class="row full">
+            <div class="large-12 medium-12 small-12 columns">
+                <div class="logo_footer text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/logo_footer.png" alt="">
+                </div>
+
+                <div class="informacion">
+                    <ul>
+                        <li>proyectopuestosyranchos@gmail.com</li>
+                        <li>+569 56650624 /@puestosyranchos</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </footer>
 
 <?php wp_footer(); ?>
@@ -8,29 +22,6 @@
     <script src="<?php bloginfo('template_directory'); ?>/bower_components/foundation/js/foundation.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/app.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
-    <script>
-        $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 15,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: true
-                },
-                600: {
-                    items: 3,
-                    nav: false
-                },
-                1000: {
-                    items: 4,
-                    nav: true,
-                    loop: false
-                }
-            }
-        })
-    </script>
-
 
 </body>
 
--- index.php
+++ index.php
@@ -1,5 +1,269 @@
 <?php get_header(); ?>
     <section>
 
+        <div class="row full collapse" id="contenedor_nosotros">
+            <div class="large-12 medium-12 small-12 columns">
+                <div class="large-6 medium-6 small-12 columns contenedor_img">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/img_rancho.jpg" alt="">
+                </div>
+                <div class="large-6 medium-6 small-12 columns">
+
+                    <div class="texto">
+                        <h5>Patrimonio arquitectónico rural</h5>
+                        <h2>Puestos y ranchos <br>
+                            de Magallanes </h2>
+                        <p>Es un proyecto que tiene como espíritu entregar a la comunidad una plataforma de búsqueda como también generar colectivamente un catastro-catálogo digital de puestos y ranchos asociados al patrimonio cultural industrial ganadero y forestal de Magallanes. Estas construcciones representan parte de la arquitectura rural de la Región de Magallanes y de La Antártica Chilena. </p>
+
+                        <a href="#" class="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row full collapse">
+            <div class="large-12 medium-12 small-12 columns">
+                <img src="<?php bloginfo('template_directory'); ?>/img/banner_separador.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="row full" id="contenedor_puestos_ranchos">
+            <div class="large-11 medium-10 small-12 columns large-centered medium-centered contendor_item">
+                <ul class="large-block-grid-3  medium-block-grid-2 small-block-grid-2">
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="puesto">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> Pingo</h3>
+                                        <span class="lugar">Tierra del Fuego</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="rancho">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> El Límite</h3>
+                                        <span class="lugar">Torres del Paine - Última esperanza</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto_2.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="puesto">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> El Chingue</h3>
+                                        <span class="lugar">Torres del Paine - Última esperanza</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto_3.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="puesto">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> Pingo</h3>
+                                        <span class="lugar">Tierra del Fuego</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="rancho">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> El Límite</h3>
+                                        <span class="lugar">Torres del Paine - Última esperanza</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto_2.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="puesto">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> El Chingue</h3>
+                                        <span class="lugar">Torres del Paine - Última esperanza</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto_3.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="puesto">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> Pingo</h3>
+                                        <span class="lugar">Tierra del Fuego</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="rancho">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> El Límite</h3>
+                                        <span class="lugar">Torres del Paine - Última esperanza</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto_2.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <div class="contenedor">
+                                <div class="item">
+
+                                    <div class="puesto_rancho">
+                                        <span class="puesto">Puesto</span>
+                                    </div>
+
+                                    <div class="texto">
+                                        <h3><i class="fa-solid fa-location-dot"></i> El Chingue</h3>
+                                        <span class="lugar">Torres del Paine - Última esperanza</span>
+                                    </div>
+
+                                    <span class="degradado"></span>
+
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/img_puesto_3.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+            <div class="large-12 medium-12 small-12 columns">
+                <div class="text-center">
+                    <a href="#" class="button">Ver más puestos y ranchos</a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="row" id="logos_colaboradores">
+            <div class="large-12 medium-12 small-12 columns large-centered medium-centered">
+                <ul class="large-block-grid-4  medium-block-grid-4 small-block-grid-4">
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/img/logo_gobierno.jpg" alt="">
+                    </li>
+
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/img/logo_patrimonio_proyecto.jpg" alt="">
+                    </li>
+
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/img/logo_RidePatagonia.jpg" alt="">
+                    </li>
+
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/img/logo_mani_producciones.jpg" alt="">
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+
+
     </section>
 <?php get_footer(); ?>
