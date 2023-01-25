2a5a8b9ef74bbd0d7c742f4e84e8539c0cc9fb3b
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-01-25 11:40:19 -0300

    Slides

--- page-agregar-ficha.php
+++ page-agregar-ficha.php
@@ -10,25 +10,27 @@ get_header("interior");
       </div>
   </div>
   <div class="row">
-    <div class="large-11 medium-11 small-12 columns large-centered medium-centered">
-      <?php
-      while ( have_posts() ) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
+    <div class="texto_interior">
+      <div class="large-11 medium-11 small-12 columns large-centered medium-centered">
         <?php
-        the_content();
-        $args = [
-          'new_post'        => [
-            'post_type'   => 'puestos_ranchos',
-            'post_status' => 'pending'
-          ],
-          'post_id'         => 'new_post',
-          'post_title'      => false,
-          'submit_value'    => 'Enviar',
-          'updated_message' => 'Contacto actualizado',
-        ];
-        acf_form( $args );
-      endwhile;
-      ?>
+        while ( have_posts() ) : the_post(); ?>
+          <h2><?php the_title(); ?></h2>
+          <?php
+          the_content();
+          $args = [
+            'new_post'        => [
+              'post_type'   => 'puestos_ranchos',
+              'post_status' => 'pending'
+            ],
+            'post_id'         => 'new_post',
+            'post_title'      => false,
+            'submit_value'    => 'Enviar',
+            'updated_message' => 'Contacto actualizado',
+          ];
+          acf_form( $args );
+        endwhile;
+        ?>
+      </div>
     </div>
   </div>
 </section>
