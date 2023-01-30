e6c6c17e107841ea0aea897171989f84f3f8cd7f
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-01-30 09:22:10 -0300

    Ficha y Mapa

--- single-puestosyranchos.php
+++ single-puestosyranchos.php
@@ -5,10 +5,8 @@
                 <img src="<?php bloginfo('template_directory'); ?>/img/banner_separador.jpg" alt="">
             </div>
         </div>
-
         <div class="row">
             <div class="large-11 medium-11 small-12 columns large-centered medium-centered">
-
                 <div class="row">
                     <div class="texto_interior">
                         <div class="large-6 medium-6 small-12 columns contendeor_img">
@@ -26,16 +24,14 @@
                                 </li>
                             </ul>
                         </div>
-
                         <div class="large-6 medium-6 small-12 columns">
                             <div class="titulo">
-                                <h3><i class="fa-solid fa-location-dot"></i> Límite</h3> <span class="rancho">Rancho</span>
-
-                                <h5 class="otros_nombres">Otros nombres</h5>
+                                <h3><i class="fa-solid fa-location-dot"></i> <?php echo get_field("nombre");?></h3> 
+                                <span class="rancho"><?php echo get_field("tipo_de_construccion");?></span>
+                                <h5 class="otros_nombres">Otros nombres: <?php echo get_field("otros_nombres");?></h5>
                             </div>
-                            <p>Memoria histórica: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut pers</p>
+                            <p>Memoria histórica: <?php echo get_field("memoria_historica");?></p>
                         </div>
-
                     </div>
                 </div>
 
@@ -46,46 +42,53 @@
                             <tbody>
                                 <tr>
                                     <th width="500">Comuna:</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("comun");?></td>
                                 </tr>
                                 <tr>
                                     <th width="500">Provincia:</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("provincia");?></td>
                                 </tr>
                                 <tr>
                                     <th width="500">Mapa:</th>
                                     <td>
-                                        <div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2392.41009519422!2d-70.89923992275055!3d-53.156680755572616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb2637f592fc0d9%3A0x1432269e02dd9821!2sCroop%20Agencia%20Digital!5e0!3m2!1ses!2scl!4v1673551775817!5m2!1ses!2scl' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe></div>
+                                      <?php 
+                                      $location = get_field('ubicacion');
+                                      if( $location ): ?>
+                                          <div class="acf-map" data-zoom="10">
+                                              <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+                                          </div>
+                                      <?php endif; ?>
+                                        <!-- <div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2392.41009519422!2d-70.89923992275055!3d-53.156680755572616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb2637f592fc0d9%3A0x1432269e02dd9821!2sCroop%20Agencia%20Digital!5e0!3m2!1ses!2scl!4v1673551775817!5m2!1ses!2scl' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe></div> -->
                                     </td>
                                 </tr>
 
                                 <tr>
                                     <th width="500">Nombre predio:</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("predio");?></td>
                                 </tr>
                                 <tr>
                                     <th width="500">Uso original:</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("uso_original_del_puesto_o_rancho");?></td>
                                 </tr>
                                 <tr>
                                     <th width="500">Año construcción:</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("anio_construccion");?></td>
                                 </tr>
                                 <tr>
                                     <th width="500">Materialidad:</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("materialidad");?></td>
                                 </tr>
                                 <tr>
-                                    <th width="500">Dimenciones:</th>
-                                    <td>x</td>
+                                    <th width="500">Dimensiones:</th>
+                                    <td><?php echo get_field("tamanio");?> mts2.</td>
                                 </tr>
                                 <tr>
-                                    <th width="500">Habitabilidad actua:</th>
-                                    <td>-</td>
+                                    <th width="500">Habitabilidad actual:</th>
+                                    <td><?php echo get_field("habitabilidad_actual");?></td>
                                 </tr>
                                 <tr>
                                     <th width="500">Uso actual</th>
-                                    <td>-</td>
+                                    <td><?php echo get_field("uso_actual");?></td>
                                 </tr>
 
                         </table>
@@ -98,4 +101,4 @@
         </div>
     </section>
 
-<?php get_footer(); ?>
+<?php get_footer("ficha"); ?>
