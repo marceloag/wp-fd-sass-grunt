3e0eec029f04d44a70cd0d3d54196f9c390f4114
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-02-15 17:11:42 -0300

    bugfix: Logos footer interior

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- FOOTER -->
       <div class="row" id="logos_colaboradores">
-          <div class="large-12 medium-12 small-12 columns large-centered medium-centered">
+          <div class="large-10 medium-10 small-12 columns large-centered medium-centered">
               <ul class="large-block-grid-4  medium-block-grid-4 small-block-grid-4">
                   <li>
                       <img src="<?php bloginfo('template_directory'); ?>/img/logo_patrimonio_proyecto.jpg" alt="Servicio de Patrimonio">
